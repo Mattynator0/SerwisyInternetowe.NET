ca1e852fcfae12ddf08fbc241ce8c8472cc344fc
Hubert Szymczak <hubert.szymczak02@gmail.com>
2025-12-01 14:15:09 +0100

    Add dashboard view and dockerized setup

--- MainApplication/Program.cs
+++ MainApplication/Program.cs
@@ -3,27 +3,39 @@ using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var mqttHost = builder.Configuration["Mqtt:Host"] ?? "mqtt";
+var mqttPort = builder.Configuration.GetValue<int?>("Mqtt:Port") ?? 1883;
+
+var mongoConnectionString = builder.Configuration["Mongo:ConnectionString"] ?? "mongodb://mongo:27017";
+var mongoDbName = builder.Configuration["Mongo:Database"] ?? "sensorsDb";
+var mongoCollectionName = builder.Configuration["Mongo:Collection"] ?? "readings";
+
 builder.Services.AddSingleton<MqttMongoService>(_ => new MqttMongoService(
-    mqttHost: "localhost",
-    mqttPort: 1883,
-    mongoConnectionString: "mongodb://localhost:27017",
-    mongoDbName: "sensorsDb",
-    mongoCollectionName: "readings"
+    mqttHost: mqttHost,
+    mqttPort: mqttPort,
+    mongoConnectionString: mongoConnectionString,
+    mongoDbName: mongoDbName,
+    mongoCollectionName: mongoCollectionName
 ));
 
 builder.Services.AddHttpClient("ApiClient", client =>
 {
-    client.BaseAddress = new Uri("http://localhost:5000/");
+    var baseUrl = builder.Configuration["Backend:BaseUrl"] ?? "http://localhost:5000/";
+    client.BaseAddress = new Uri(baseUrl);
 });
+
 builder.Services.AddSingleton<ISensorDataService>(sp =>
 {
-    var mongoClient = new MongoClient("mongodb://localhost:27017");
-    var database = mongoClient.GetDatabase("sensorsDb");
-    var collection = database.GetCollection<SensorData>("readings");
+    var mongoClient = new MongoClient(mongoConnectionString);
+    var database = mongoClient.GetDatabase(mongoDbName);
+    var collection = database.GetCollection<SensorData>(mongoCollectionName);
     return new SensorDataService(collection);
 });
+
 builder.Services.AddControllers();
 builder.Services.AddRazorPages();
+
+builder.WebHost.UseUrls("http://0.0.0.0:5000");
 
 var app = builder.Build();
 
@@ -35,4 +47,5 @@ app.UseRouting();
 app.MapRazorPages();
 app.MapControllers();
 
+app.Run();
 app.Run();
