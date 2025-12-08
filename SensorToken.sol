8c9a3920f55c3d2113011e9608b252bbbbf7da9a
Piotr Kolasiński <160526901+Hil4ri0n@users.noreply.github.com>
2025-12-08 14:29:15 +0100

    Refactor SensorToken contract to remove Ownable
    Removed Ownable inheritance and minting function.

--- SensorToken.sol
+++ SensorToken.sol
@@ -2,15 +2,9 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SensorToken is ERC20, Ownable {
-    constructor() ERC20("Sensor Token", "STKN") Ownable(msg.sender) {}
-
-    /// @notice Nagradza sensor tokenami (mint nowych tokenów)
-    /// @param sensor adres portfela sensora
-    /// @param amount iloœæ tokenów (w najmniejszych jednostkach, np. 1e18 = 1 STKN)
-    function rewardSensor(address sensor, uint256 amount) external onlyOwner {
-        _mint(sensor, amount);
+contract SensorToken is ERC20 {
+    constructor() ERC20("Sensor Token", "STKN") {
+        _mint(msg.sender, 1_000_000 * 10 ** decimals());
     }
 }
